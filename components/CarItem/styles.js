cbc2115b978eb63158702d88143de18e9c4f5df4
Yui Tung Lee <17228336@life.hkbu.edu.hk>
2021-09-11 22:52:35 +0800

    Car item component

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -5,25 +5,39 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+
     titles: {
         marginTop: '30%',
         width: '100%',
         alignItems: 'center',
     },
+
     title: {
         fontSize: 40,
         fontWeight: '500'
     },
+
+    subtitleCTA: {
+        textDecorationLine: 'underline'
+    },
+
     subtitle: {
         fontSize: 16,
         color: '#5c5e62'
     },
+
     image: {
         width: '100%',
         height: '100%',
         resizeMode: 'cover',
         position: 'absolute'
-    }
+    },
+
+    buttonsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '100%'
+    },
 });
 
 export default styles;
